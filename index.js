95f5e95c5e9348c471ceeab027fa12826c5a1be3
Joseph Lorenzo <josephclorenzo@gmail.com>
2018-10-12 03:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// function myAnimal() {
-//   var animal = 'dog';
-//   return animal;
-// }
+function myAnimal() {
+  var animal = 'dog';
+  return animal;
+}
 
 function yourAnimal(animal) {
   return animal;
@@ -9,22 +9,22 @@ function yourAnimal(animal) {
 
 yourAnimal("cat");
 
-// function add2(n) {
-//   const two = 2;
-//   return n + two;
+function add2(n) {
+  const two = 2;
+  return n + two;
 
-// }
+}
 
-// var funkyFunction = function() {
-//   return function() {
-//     return "FUNKY!";
-//   };
-// };
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!";
+  };
+};
 
-// // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// // NOTE: you only need to modify the code below this line.
-// var theFunk = funkyFunction();
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+var theFunk = funkyFunction();
 
-// add2(837);
-// myAnimal();
-// theFunk();
+add2(837);
+myAnimal();
+theFunk();
